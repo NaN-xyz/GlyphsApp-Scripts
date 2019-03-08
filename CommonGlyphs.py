421ca697fe144aa5b2373a5f1c3b51887eda8a0b
Luke Prowse <47065184+LukeProwse@users.noreply.github.com>
2019-03-08 12:14:05 +0100

    Add files via upload

---
+++ CommonGlyphs.py
@@ -0,0 +1,49 @@
+#MenuTitle: Common glyphs between n-open fonts
+# -*- coding: utf-8 -*-
+__doc__="""
+Prints glyphs common to all open fonts to the macro panel.
+"""
+
+Glyphs.showMacroWindow()
+Glyphs.clearLog()
+
+allfonts = Glyphs.fonts
+allglyphs = list()
+ofonts=[]
+
+if len(allfonts) > 0: 
+
+	for f in allfonts:
+		niceglyphs = []
+		ofonts.append(f.familyName)
+		for g in f.glyphs: 
+			niceglyphs.append(g.name) # see g.string and g.unicode
+		allglyphs.append(niceglyphs)
+
+	allglyphs.sort() 
+	temp = allglyphs[0]
+	for f in range(1,len(allglyphs)): temp = set(temp).intersection(allglyphs[f])
+
+	print "Analyised fonts: " + ", ".join(ofonts)
+	print "Number of common glyphs: " + str(len(temp)) + "\n"
+	
+	nice, prod, simp = "", "", ""
+	string =""
+
+	for p in temp: 
+		nice+=p+" "
+		prod+=Glyphs.productionGlyphName(p)+" "
+		string = Glyphs.fonts[0].glyphs[p].string
+		if string:
+			string.encode('unicode_escape')
+			simp+=string+" "
+
+	print simp
+	print "\n"
+	print "Nice names: \n" + nice
+	print "\n"
+	print "Production names: \n" + prod
+	print "\n"
+
+else:
+	print "Insufficient number of fonts open to compare."
